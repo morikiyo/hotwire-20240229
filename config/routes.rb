f70fd212add702494524346c6cbb58c98c93ad0a
Morikiyo INOHA <morikiyo@sonicgarden.jp>
2024-02-29 17:34:55 +0900

    add dashboard

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  resource :dashboard, only: [:show]
   resources :members
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
